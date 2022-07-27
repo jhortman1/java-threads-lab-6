9168d42d2f15f89aa59bb8fb9ffcae6f6be61267
James Hortman <jamesdhortman@gmail.com>
2022-07-27 10:10:27 -0700

    Threads lab 6 complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,21 @@
-import java.util.Scanner;
-import java.util.concurrent.*;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // create an executor
-        ExecutorService executor; 
+        ExecutorService executor = Executors.newCachedThreadPool();
+
 
         while (scanner.hasNext()) {
             int num = scanner.nextInt();
             // submit tasks to your executor
+            executor.submit(new PrimeLogger(num));
         }
+        executor.shutdown();
+        scanner.close();
     }
 }
 
@@ -24,5 +29,10 @@ class PrimeLogger implements Runnable {
     @Override
     public void run() {
         // print num if it is prime
+        List<Long> nums = new ArrayList<>();
+        for (long x = 2; x * x <= num; x ++){
+            nums.add(x);
+        }
+        if(!nums.stream().anyMatch((x) -> num % x == 0)) System.out.printf("%s is prime \n",num);
     }
 }
